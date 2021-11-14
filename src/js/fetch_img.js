9350fe3c7036dbac91e96212e7bfb1d1f4f19190
omykhalska <83216292+omykhalska@users.noreply.github.com>
2021-11-14 03:03:49 +0200

    Добавляет функции для http-запросов и рендеринга галлереи

--- src/js/fetch_img.js
+++ src/js/fetch_img.js
@@ -1,13 +1,11 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api';
 const KEY = '22840960-ea2b07fd8d407a17e77cd52c1';
 
-export function fetchImages(query) {
-  return fetch(
+export async function fetchImages(query) {
+  const response = await axios.get(
     `${BASE_URL}/?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`,
-  ).then(response => {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  });
+  );
+  return response;
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,62 @@
-import '../css/styles.css';
 import { fetchImages } from './fetch_img.js';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+import '../css/styles.css';
+
 const formEl = document.querySelector('#search-form');
+const galleryEl = document.querySelector('.gallery');
 
-formEl.addEventListener('submit', e => {
+formEl.addEventListener('submit', onSearchSubmit);
+
+// const gallery = new SimpleLightbox('.gallery a');
+
+/*--------FUNCTIONS----------*/
+function onSearchSubmit(e) {
   e.preventDefault();
+  galleryEl.innerHTML = '';
+  getImages();
+}
 
+function getImages() {
   const query = formEl.elements.searchQuery.value.trim();
   fetchImages(query)
-    .then(data => {
+    .then(({ data }) => {
       if (data.hits.length === 0) {
         Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
       }
-      console.log(data);
+      renderGallery(data.hits);
     })
-    .catch(error => console.log(error.message));
-});
+    .catch(error => console.log(error));
+}
+
+function createMarkup(images) {
+  return images
+    .map(image => {
+      const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = image;
+      return `<div class="photo-card">
+      <a href="${largeImageURL}">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300"/>
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b> ${likes}
+            </p>
+            <p class="info-item">
+              <b>Views</b> ${views}
+            </p>
+            <p class="info-item">
+              <b>Comments</b> ${comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads</b> ${downloads}
+            </p>
+          </div>
+          </a>
+        </div>`;
+    })
+    .join('');
+}
+
+function renderGallery(images) {
+  galleryEl.insertAdjacentHTML('beforeend', createMarkup(images));
+}
